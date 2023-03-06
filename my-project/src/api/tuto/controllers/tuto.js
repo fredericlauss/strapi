655b20aaf457faf8e17244edc942886860025cce
frederic <flausson@normandiewebschool.fr>
2023-03-06 14:52:56 +0100

    init project

---
+++ my-project/src/api/tuto/controllers/tuto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tuto controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tuto.tuto');
---
+++ my-project/src/api/tuto/routes/tuto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tuto router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tuto.tuto');
